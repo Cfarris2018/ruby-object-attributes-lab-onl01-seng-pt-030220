256eceb319c5b7e9f78fa2b8826546a053f17b6e
Camisha  Farris <cfarris@anointedflooring.com>
2020-03-24 06:02:31 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person
+  
+  def name= person_name
+    @person_name= name
+  end
+  
+  def name
+    @person_name
+  end
+  
+  
+  def job= job_name
+    @job_name= name
+  end
+  
+  def job
+    @job_name
+  end
+
+end
+
